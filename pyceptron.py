d07dcd4686813fd280ea94d01baaec7aa5606b6a
Olivia Guest <guest.olivia@gmail.com>
2014-04-14 21:57:22 +0100

    minor edit

--- pyceptron.py
+++ pyceptron.py
@@ -33,7 +33,7 @@ white = [255, 255 , 255]
  
 class Unit(object):
   "A neural network unit: represented by a circle"
-  def __init__(self, layer, i, x_offset, y_offset, colour = [255, 255 , 255], activation = 0.0, bias = 0.001, radius = 60, x_spacing = 150, y_spacing = 150):
+  def __init__(self, layer, i, x_offset, y_offset, colour = black, activation = 0.0, bias = 0.001, radius = 60, x_spacing = 150, y_spacing = 150):
     self.layer = layer
     self.i = i
     self.colour = colour
@@ -49,18 +49,18 @@ class Unit(object):
 
     
   def Draw(self):
-    pygame.draw.circle(screen, (self.colour[0], self.colour[1], self.colour[2]), (self.x, self.y), self.radius)
+    if self.activation > 0.0:
+      width = 2
+    else:
+      width = 0
+    pygame.draw.circle(screen, (self.colour[0], self.colour[1], self.colour[2]), (self.x, self.y), self.radius, width)
 
   def Colour(self):
-    #if self.activation == 1:
-      #self.colour = purple
-    #else:
-      #self.colour = white
     self.colour[1] = 255*self.activation
 
   def Clamp(self, value):
     self.activation =  value
-    self.Colour()
+    #self.Colour()
     self.Draw()
 
   def Update(self):
@@ -73,7 +73,7 @@ class Unit(object):
       self.activation = 1
     else:
       self.activation = 0
-    self.Colour()
+    #self.Colour()
     self.Draw()
     self.in_activations = 0
     #print self.activation
@@ -82,7 +82,7 @@ class Unit(object):
 
 class Weight(object):
   "A neural network connection weight: reprsented by a line"
-  def __init__(self, unit_from, unit_to, colour = black, strength = 0.05):
+  def __init__(self, unit_from, unit_to, colour = black, strength = 0.0):
     self.unit_from = unit_from
     self.unit_to =  unit_to
     self.colour = colour
@@ -102,22 +102,28 @@ class Weight(object):
     self.unit_to.in_activations += self.unit_from.activation * self.strength
 
   def Update(self, target, learning_rate):
-    self.strength += learning_rate * (target - self.unit_to.activation)
-    print"Output error:", target - self.unit_to.activation,
+    error = target - self.unit_to.activation
+    self.strength += learning_rate * error
+    print"Output error:", error,
     print "Weight:", self.strength
     self.Draw()
+    return error
     
 
 class Network(object):
   "The network itself!"
-  def __init__(self, units, patterns, targets, learning_rate = 0.001, height = 600, width = 600, background = 0):
-    #create list to hold units in a neural network layer as another to keep the weights
+  def __init__(self, patterns, targets,  units = [1, 1], learning_rate = 0.001, height = 600, width = 600, background = 0):
     self.units = units
+    self.patterns = patterns
+    self.targets = targets
+
+    if len(patterns[0]) != units[0]:
+      self.units[0] = len(patterns[0])
+    if len(targets[0]) != units[-1]:
+      self.units[-1] = len(targets[0])
     self.layers =  len(self.units)
     self.layer = [None] * self.layers
     self.weights = [None] * self.layers
-    self.patterns = patterns
-    self.targets = targets
     self.learning_rate = learning_rate
     self.background = background
     for l in range(self.layers):
@@ -129,7 +135,7 @@ class Network(object):
 	  self.weights[l][unit_on_prev_layer] =  [None] * self.units[l]
 
 	  
-    radius = int(height/40)
+    radius = int(height/(max(units)*3))
     x_spacing = int(radius*1.5)
     y_spacing = int(radius*1.5)
     x_offset = int((width - (self.layers - 1) * (x_spacing + radius)) / 2.0)
@@ -139,7 +145,7 @@ class Network(object):
       for unit_on_this_layer in range(self.units[l]):
 	#cycle through the units to actually create the units for the layer we are on
 	y_offset = int((height - ((self.units[l]-1) * (y_spacing + radius))) / 2.0)
-	self.layer[l][unit_on_this_layer] = Unit(l, unit_on_this_layer, x_offset, y_offset, white, 0.0, 0.001, radius, x_spacing, y_spacing)
+	self.layer[l][unit_on_this_layer] = Unit(l, unit_on_this_layer, x_offset, y_offset, black, 0.0, 0.001, radius, x_spacing, y_spacing)
 	self.layer[l][unit_on_this_layer].Draw()
 
 	if l > 0:
@@ -151,7 +157,10 @@ class Network(object):
 
 
   def Train(self):
-    for x in range(1000):
+    error = 1.0
+    #for x in range(1000):
+    while error != 0.0:
+      error = 0.0
       #clamp the input layer to a pattern p
       for p in range(len(self.patterns)):
         screen.blit(self.background, (0, 0))
@@ -170,21 +179,32 @@ class Network(object):
 	  
 	for unit_on_this_layer in range(self.units[1]): 
 	  for unit_on_prev_layer in range(self.units[0]):
-	    self.weights[1][unit_on_prev_layer][unit_on_this_layer].Update(self.targets[p][unit_on_this_layer], self.learning_rate)
+	    error += self.weights[1][unit_on_prev_layer][unit_on_this_layer].Update(self.targets[p][unit_on_this_layer], self.learning_rate)
        
 	pygame.display.update()
+	
+	
 
-	time.sleep(0.5)
+	#time.sleep(0.5)
 
-Units = [10, 2]
-Patterns = [[0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-Targets = [[0.0, 0.0]]
+Patterns = [
+	    [1.0, 0.0],
+	    [0.0, 1.0],
+	    [0.0, 0.0],
+	    [1.0, 1.0]
+	   ]
+Targets = [
+	    [1.0],
+	    [1.0],
+	    [0.0],
+	    [1.0]
+	  ]
 #Patterns = [[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [0.0, 1.0, 1.0], [1.0, 1.0, 0.0]]
 #Targets = [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]
 
 def Main():
   #below this line are things that will be run - above it are just declarations and definitions of classes, etc.
-  N = Network(Units, Patterns, Targets, background = background)
+  N = Network(Patterns, Targets, background = background)
 
   N.Train()
 	
