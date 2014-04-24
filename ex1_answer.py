c6f918e6b1cd91e7e3736d5528a558ef4d5fea2b
Olivia Guest <guest.olivia@gmail.com>
2014-04-24 08:40:57 +0100

    tidying

--- ex1_answer.py
+++ ex1_answer.py
@@ -1,67 +1,79 @@
-from math import pi
-import random as r
-import copy as cop
-import time
+from pyceptron import *
 
-import pygame
-from pygame.locals import *
 
-import pyceptron
+# Uncomment one of the followling patterns and targets. If you don't the network will be trained and run on a default pattern. One of these patterns is problematic, why?  ~ Olivia
 
-#preset colours
-magenta = [255, 0, 255]
-black = [0, 0 , 0]
-white = [255, 255 , 255]
-blue = [0, 0, 255]
-red = [255, 0, 0]
-green = [0, 255, 0]
-yellow = [255, 255, 0]
 
-height = 200
-width = 200
-#initialise pygame
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("My unit!")
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill(magenta)
-screen.blit(background, (0, 0))
 
-#varibles for position and size of our unit
-radius = int(height/10)
-x_spacing = int(radius*2.5)
-y_spacing = int(radius*2.5)
-x_offset = int(width / 2.0)
-y_offset = int(height / 2.0)
 
-#here we are defining our unit
-My_unit = pyceptron.Unit(0, 0, x_offset, y_offset, colour = white, radius = 40)
 
-for i in range(10):
-  My_unit.colour = white
-  My_unit.Draw()    
-  pygame.display.update()
+
+#Patterns = [ ## NOT ##
+            #[0.0],
+            #[1.0],
+           #]
+
+#Targets = [
+           #[1.0], #first target, corresponds to first pattern
+           #[0.0],
+         
+          #]
+          
+          
+#Patterns = [ ## OR ## 
+            #[0.0, 0.0],
+            #[0.0, 1.0],
+            #[1.0, 0.0],
+            #[1.0, 1.0]
+           #]
+
+#Targets = [
+           #[0.0], #first target, corresponds to first pattern
+           #[1.0],
+           #[1.0],
+           #[1.0],
+         
+          #]
+          
+          
+#Patterns = [ ## AND ##
+            #[0.0, 0.0],
+            #[0.0, 1.0],
+            #[1.0, 0.0],
+            #[1.0, 1.0]
+           #]
+
+#Targets = [
+           #[0.0], #first target, corresponds to first pattern
+           #[0.0],
+           #[0.0],
+           #[1.0],
+         
+          #]  
+
+          
+#Patterns = [ ## XOR ## 
+            #[0.0, 0.0],
+            #[0.0, 1.0],
+            #[1.0, 0.0],
+            #[1.0, 1.0]
+           #]
+
+#Targets = [
+           #[0.0], #first target, corresponds to first pattern
+           #[1.0],
+           #[1.0],
+           #[0.0],
+         
+          #]
+          
+          
+N = Network(Patterns, Targets)
+
+N.Train()
+
+N.Run()
   
-  #try commenting these following three lines to see what they do
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      quit()
-  
-  #ditto
-  time.sleep(0.5)
-
-  My_unit.colour = black
-  My_unit.Draw()    
-  pygame.display.update()
-  
-  #ditto
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      quit()
-    
-  time.sleep(0.5)
-
 
 #loop to fall into once the main stuff has been done
 while (1):
--- pyceptron.py
+++ pyceptron.py
@@ -2,164 +2,33 @@ from math import *
 import random 
 import copy as cop
 import time
-
-#import pygame
+import matplotlib
+import numpy
 import pygame
 from pygame.locals import *
-
 
 def get_angle(x1, y1, x2, y2):
   dx = x2 - x1
   dy= y2 - y1  
   return atan2(dy,dx)
 
-
-
 #preset colours
 magenta = [255, 0, 255]
 black = [0, 0 , 0]
 white = [255, 255 , 255]
 
+#f = lambda x: 0.0 if x < 0.0 else 1.0
+def f(x):
+  if x < 0.0:
+    return 0.0
+  else:
+    return 1.0
 
-
- 
-class Unit(object):
-  "A neural network unit: represented by a circle"
-  def __init__(self, layer, i, x_offset, y_offset, colour = black, activation = 0.0, radius = 60, x_spacing = 150, y_spacing = 150, screen = None):
-    self.layer = layer
-    self.i = i
-    self.colour = colour
-    self.radius = radius
-    self.x = self.layer * (x_spacing+self.radius) + x_offset
-    self.y = self.i * (y_spacing+self.radius)  + y_offset
-    self.x_spacing = x_spacing
-    self.y_spacing = y_spacing
-    self.activation = activation
-    self.in_weights = 0.0
-    self.in_activations = 0.0
-    self.screen = screen
-    #self.prev_activation = 0.0
-    #self.bias = -2     
-
-    
-  def Draw(self):
-    #if self.activation > 0.0:
-      #width = 2
-      #self.colour[0] *= self.activation
-      #self.colour[1] *=self.activation
-      #self.colour[2] *=self.activation
-    #else:
-    width = 2
-      #self.colour = black  
-
-    #print self.i, self.layer,  self.activation  
-    pygame.draw.circle(self.screen, (self.colour[0], self.colour[1], self.colour[2]), (self.x, self.y), self.radius, width)
-
-    #font=pygame.font.Font(None,10) 
-    #scoretext=font.render(str(self.prev_activation), 1,magenta)
-    font=pygame.font.Font(None,50) 
-
-    text=font.render(str(self.activation), 1,black)
-    self.screen.blit(text, (self.x-25, self.y-20))
-
-    
-  def Colour(self):
-    self.colour[1] = 255*self.activation
-
-  def Clamp(self, value):
-    self.activation =  value
-    #self.Colour()
-    self.Draw()
-
-  def Update(self):
-    #print self.i
-    #print self.j
-    #print self.activation
-    #print self.in_activations
-    #self.prev_activation = self.activation
-    self.activation = self.in_activations - self.bias.strength
-    if self.activation  > 0:
-      self.activation = 1.0
-    else:
-      self.activation = 0.0
-    #self.Colour()
-    self.Draw()
-    self.in_activations = 0
-    print "Unit activation = ", self.activation,
-    print self.in_activations, self.bias.strength
-    
-  def Set_bias(self, bias):
-    self.bias = bias
-       
-
-class Weight(object):
-  "A neural network connection weight: represented by a line"
-  def __init__(self, unit_from, unit_to, colour = black, strength = 0.001, screen = None):
-    self.unit_from = unit_from
-    self.unit_to =  unit_to
-    self.colour = colour
-    self.strength = strength
-    self.screen = screen
-    self.d_strength = 0.0
-
-  def Draw(self):
-    if self.unit_from:
-      x1 = self.unit_from.x
-      y1 = self.unit_from.y
-      x2 = self.unit_to.x
-      y2 = self.unit_to.y
-      r1 = self.unit_from.radius
-      r2 = self.unit_to.radius
-      a = get_angle(x1, y1, x2, y2)
-      pygame.draw.line(self.screen, self.colour, (x1 + r1 * cos(a),  y1 + r1 * sin(a)), (x2 - r2 * cos(a), y2 - r2 * sin(a)), 1)
-
-
-  def Propagate(self):
-    "Send activations from self.unit_from to self.unit_to"
-    self.unit_to.in_activations += self.unit_from.activation * self.strength
-
-  def Update(self, target, learning_rate, pattern=None, batch = True):
-    error = target - self.unit_to.activation
-    if pattern ==None:
-          self.d_strength -= learning_rate * error 
-          print"Output error:", error,
-          print "Bias:", self.strength
-    else:
-          self.d_strength -= learning_rate * error * self.unit_from.activation
-          
-          print"Output error:", error,
-          print "Weight:", self.strength
-          #print "Error:", error,
-          #print "Unit from:", self.unit_from.activation
-    #self.strength += learning_rate * self.unit_to.activation
-
-    #print "Learning rate:", learning_rate
-    #print "Output activation:", self.unit_to.activation
-    self.Draw()
-    return error
-  def Apply_batch(self):
-    self.strength += self.d_strength
-    self.d_strength = 0.0
-    
-class Bias(Weight):
-  pass
-    
-  #def Update(self, target, learning_rate):
-    #error = target - self.unit_to.activation
-    #self.strength += learning_rate * error
-    ##self.strength += learning_rate * self.unit_to.activation
-    #print"Output error:", error,
-    #print "Weight:", self.strength
-    ##print "Learning rate:", learning_rate
-    ##print "Output activation:", self.unit_to.activation
-    ##self.Draw()
-    #return error
 class Network(object):
   "The network itself!"
-  def __init__(self, patterns, targets,  units = [1, 1], learning_rate = 0.015, height = 600, width = 600):
-    
+  def __init__(self, patterns, targets,  learning_rate = 0.02, height = 600, width = 600): 
 
-    #initialise pygame
+    #Initialise pygame
     pygame.init()
     self.screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Pyceptron")
@@ -168,175 +37,170 @@ class Network(object):
     self.background.fill([200, 200, 200])
     self.screen.blit(self.background, (0, 0))
 
-
-
-
-    self.units = units
+    # Initialising network
+    self.input_units =  numpy.ones(len(patterns[0])+1)
+    self.output_units = numpy.zeros(len(targets[0]))
     self.patterns = patterns
     self.targets = targets
-
-    if len(patterns[0]) != units[0]:
-      self.units[0] = len(patterns[0])
-    if len(targets[0]) != units[-1]:
-      self.units[-1] = len(targets[0])
-    self.layers =  len(self.units)
-    self.layer = [None] * self.layers
-    self.weights = [None] * self.layers
-    self.biases = [None] * (self.layers)
     self.learning_rate = learning_rate
-    for l in range(self.layers):
-      self.layer[l] = [None] * self.units[l]
-      if l > 0:
-        #we want weights to only exist between two layers, so the 0th layer on its own cannot have any connections
-        self.weights[l] = [None] * self.units[l-1]
-        for unit_on_prev_layer in range(self.units[l-1]):
-          self.weights[l][unit_on_prev_layer] =  [None] * self.units[l]
-        self.biases[l] = [None] * self.units[l]
-
+    self.weights = numpy.zeros(len(self.input_units))
+    self.errors = []
+    self.learning_rate = 0.2
+    self.layers = 2
   
+    # GUI stuff
+    self.radius = int( height/ (3* max(len(self.input_units), len(self.output_units)) ))
+    self.x_spacing = int(self.radius*1.5)
+    self.y_spacing = int(self.radius*1.5)
+    self.x_offset = int((width - (self.layers - 1) * (self.x_spacing + self.radius)) / 2.0)
+    self.input_units_x =  numpy.ones(len(self.patterns[0]))
+    self.input_units_y =  numpy.ones(len(self.patterns[0]))
+    self.output_units_x =  numpy.ones(len(self.output_units))
+    self.output_units_y =  numpy.ones(len(self.output_units))
+    
+    for i in range(len(self.patterns[0])):
+      y_offset = int((height - ((len(self.patterns[0])-1) * (self.y_spacing + self.radius))) / 2.0)
+      self.input_units_x[i] = 0 * (self.x_spacing+self.radius) + self.x_offset     # constant represents what layer they are on
+      self.input_units_y[i] = i * (self.y_spacing+self.radius)  + y_offset
 
-    radius = int( height/ (3* max( max( self.units ), self.layers) ))
-    x_spacing = int(radius*1.5)
-    y_spacing = int(radius*1.5)
+    for i in range(len(self.output_units)):
+      y_offset = int((height - ((len(self.output_units)-1) * (self.y_spacing + self.radius))) / 2.0)
+      self.output_units_x[i] = 1 * (self.x_spacing+self.radius) + self.x_offset    # constant represents what layer they are on
+      self.output_units_y[i] = i * (self.y_spacing+self.radius)  + y_offset
 
-    x_offset = int((width - (self.layers - 1) * (x_spacing + radius)) / 2.0)
+    # More GUI stuff
+    self.screen.blit(self.background, (0, 0))
+    for i in range(len(self.patterns[0])):
+
+      x1 = self.input_units_x[i]
+      y1 = self.input_units_y[i]
+      x2 = self.output_units_x[0]
+      y2 = self.output_units_y[0]
+      r1 = self.radius
+      r2 = self.radius
+      a = get_angle(x1, y1, x2, y2)
+      pygame.draw.line(self.screen, black, (x1 + r1 * cos(a),  y1 + r1 * sin(a)), (x2 - r2 * cos(a), y2 - r2 * sin(a)), 1)
+      pygame.draw.circle(self.screen, black, (int(x1), int(y1)), self.radius, 2)
+
+    for i in range(len(self.output_units)):
+      x2 = self.output_units_x[i]
+      y2 = self.output_units_y[i]
+      pygame.draw.circle(self.screen, black, (int(x2), int(y2)), self.radius, 2)
+
+    pygame.display.update()
  
-    #initialisation of network
-    for l in range(self.layers):
-      for unit_on_this_layer in range(self.units[l]):
-       #cycle through the units to actually create the units for the layer we are on
-       y_offset = int((height - ((self.units[l]-1) * (y_spacing + radius))) / 2.0)
-       self.layer[l][unit_on_this_layer] = Unit(l, unit_on_this_layer, x_offset, y_offset, black, 0.0, radius, x_spacing, y_spacing, screen = self.screen)
-       self.layer[l][unit_on_this_layer].Draw()
 
-       if l > 0:
-         #we want weights to only exist between two layers, so the 0th layer on its own cannot have any connections
-          for unit_on_prev_layer in range(self.units[l-1]):
-            #cycle through the units of the previous layer so we can connect them to their counterparts on this layer
-            self.weights[l][unit_on_prev_layer][unit_on_this_layer] = Weight(self.layer[l-1][unit_on_prev_layer], self.layer[l][unit_on_this_layer], strength = random.gauss(0.5, 0.01),  screen = self.screen)
-            self.weights[l][unit_on_prev_layer][unit_on_this_layer].Draw()
-          self.biases[l][unit_on_this_layer] = Bias(None, self.layer[l][unit_on_this_layer],strength = random.gauss(0.0, 0.01))
-          self.layer[l][unit_on_this_layer].Set_bias(self.biases[l][unit_on_this_layer])
+  def Draw(self):
+    # GUI stuff
+    
+    for event in pygame.event.get():
+      if event.type == QUIT:
+        quit()
+        pygame.display.update()
+        
+    for i in range(len(self.input_units)-1):
 
-  def Run(self):
-        while True:
-          for p in range(len(self.patterns)):
-            print "Running: Input Pattern:", self.patterns[p],
-            self.screen.blit(self.background, (0, 0))
+      x1 = self.input_units_x[i]
+      y1 = self.input_units_y[i]
+      x2 = self.output_units_x[0]
+      y2 = self.output_units_y[0]
+      r1 = self.radius
+      r2 = self.radius
 
-            for event in pygame.event.get():
-              if event.type == QUIT:
-                quit()
+      rect = pygame.Rect(int(x1)-25, int(y1)-20, 50, 40)
+      pygame.draw.rect(self.screen, [200, 200, 200], rect, 0)
+      font=pygame.font.Font(None,50)
+      text=font.render(str(self.input_units[i]), 1,black)
+      self.screen.blit(text, (int(x1)-25, int(y1)-20))
+      pygame.display.update(rect)
+          
+          
+    for i in range(len(self.output_units)):
+      x2 = self.output_units_x[i]
+      y2 = self.output_units_y[i]
+      rect = pygame.Rect(int(x2)-25, int(y2)-20, 60, 40)
+      pygame.draw.rect(self.screen, [200, 200, 200], rect, 0)
+      font=pygame.font.Font(None,50)
+      text=font.render(str(self.output_units[i]), 1,black)
+      self.screen.blit(text, (int(x2)-25, int(y2)-20))
+      pygame.display.update(rect)
 
-            for unit_on_this_layer in range(self.units[0]):
-              self.layer[0][unit_on_this_layer].Clamp(self.patterns[p][unit_on_this_layer])
 
-            for unit_on_this_layer in range(self.units[1]):
-              for unit_on_prev_layer in range(self.units[0]):
-                self.weights[1][unit_on_prev_layer][unit_on_this_layer].Propagate()
-                self.weights[1][unit_on_prev_layer][unit_on_this_layer].Draw()
-            
-            for unit_on_this_layer in range(self.units[1]):
-              self.layer[1][unit_on_this_layer].Update()
-
-            print "Output:", self.layer[-1][-1].activation
-            pygame.display.update()
-            time.sleep(1)
-
-  def Train(self):
-    error = 1.0
-    #for x in range(5000):
-    #while True:
-    while error != 0.0:
-
-      error = 0.0
-      #clamp the input layer to a pattern p
-      for p in range(len(self.patterns)):
-          self.screen.blit(self.background, (0, 0))
-
-          for event in pygame.event.get():
-            if event.type == QUIT:
-              quit()
-
-          for unit_on_this_layer in range(self.units[0]):
-            self.layer[0][unit_on_this_layer].Clamp(self.patterns[p][unit_on_this_layer])
-
-          for unit_on_this_layer in range(self.units[1]):
-            for unit_on_prev_layer in range(self.units[0]):
-              self.weights[1][unit_on_prev_layer][unit_on_this_layer].Propagate()
-
-          for unit_on_this_layer in range(self.units[1]):
-            self.layer[1][unit_on_this_layer].Update()
       
-          for unit_on_this_layer in range(self.units[1]):
-              error += fabs(self.biases[1][unit_on_this_layer].Update(self.targets[p][unit_on_this_layer], self.learning_rate))
-              for unit_on_prev_layer in range(self.units[0]):
-                error += fabs(self.weights[1][unit_on_prev_layer][unit_on_this_layer].Update(self.targets[p][unit_on_this_layer], self.learning_rate, pattern =self.patterns[p][unit_on_prev_layer]))
-          #time.sleep(1)
-              print "Input:", self.patterns[p],
-              print "Target:", self.targets[p][unit_on_this_layer],
-              print unit_on_this_layer,
-              print "Output:", self.layer[1][unit_on_this_layer].activation
+  def Train(self):
+       
+        x = self.input_units 
+        w = self.weights
+        h = self.learning_rate
+        d = self.targets
+        N = len(self.patterns[0])
+        P = len(self.patterns)
+        
+        for t in range(100):
+              for p in range(P):
+                        
+                    for i in range(N):    
+                      x[i] = self.patterns[p][i]
+                    #print "x = ", x[0:2]
+                    y = 0
+                    for i in range(N+1): 
+                      y += x[i] * w[i]
 
-          pygame.display.update()
-          
-      for unit_on_this_layer in range(self.units[1]):
-          self.biases[1][unit_on_this_layer].Apply_batch()
-          for unit_on_prev_layer in range(self.units[0]):
-             self.weights[1][unit_on_prev_layer][unit_on_this_layer].Apply_batch()    
-      error /= len(self.patterns)
-      print "Network error:", error
+                    self.output_units[0] = f(y)
+                    #print "y = ", y
+                    error = d[p][0] - self.output_units[0]
+                    #print "f(y) = ", self.output_units[0]
+
+                    print "error = ", error
+                    for i in range(N+1): 
+                      w[i] += h * error * x[i]
+
+                    self.Draw()
+                    
+  def Run(self):
+    x = self.input_units 
+    w = self.weights
+    h = self.learning_rate
+    d = self.targets
+    N = len(self.patterns[0])
+    P = len(self.patterns)
+
+    while True:
+          for p in range(P):
+                  for i in range(N):
+                    x[i] = self.patterns[p][i]
+                  #print "x = ", x[0:2]
+                  y = 0
+                  for i in range(N+1):
+                    y += x[i] * w[i]
+                  self.output_units[0] = f(y)
+
+                  #print "y = ", y
+                  error = self.targets[p][0] - f(y)
+                  #print "f(y) = ", f(y)
+
+                  #print "error = ", error
+
+                  self.Draw()
 
 
+                  time.sleep(0.5)
 
 
-       #time.sleep(0.5)
+Patterns = [
+            [0.1, 0.0, 0.2], #loquat
+            [0.0, 0.2, 0.0], #lemon
+            [1.0, 0.5, 0.8], #red apple
+            [1.0, 0.0, 0.9], #strawberry
+           ]
 
-#Patterns = [
-            #[-1], #first pattern
-            #[1]
-            ##[1.0, 0.0],
-            ##[1.0, 1.0]
-           #]
-
-#Targets = [
-           #[0.0], #first target, corresponds to first pattern
-           #[1.0],
-
-           ##[1.0],
-           #[1.0]
-          #]
-          
-#Patterns=[[0.5,1.5], [-0.5, 0.5], [0.5, 0.5]] 
-#Targets=[[1.0], [0], [1]]
-          
-#Patterns = [
-            #[-1, -1], #first pattern
-            #[-1, 1.0],
-            #[1.0, -1],
-            #[1.0, 1.0]
-           #]
-
-#Targets = [
-           #[0], #first target, corresponds to first pattern
-           #[0],
-           #[0],
-           #[1.0]
-          #]
-          
-#Patterns = [
-            #[-1.0], #first pattern
-           
-            #[1.0]
-           #]
-
-#Targets = [
-          
-           #[1.0],
-           #[0.0]
-          #]
-Patterns = [[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 1.0], [0.0, 1.0, 1.0], [1.0, 1.0, 0.0]]
-Targets = [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [1.0, 1.0], [1.0, 1.0], [1.0, 1.0]]
+Targets = [
+           [1.0], #first target
+           [1.0],
+           [0.0],
+           [0.0],
+         
+          ]            
 
 def Main():
   #below this line are things that will be run - above it are just declarations and definitions of classes, etc.
